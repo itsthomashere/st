93712541d58fe4f21b5cb0d3c84bd196747acf48
BMHDAC <bdachai308@gmail.com>
2024-12-11 12:09:13 +0700

    Gruvbox

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "MapleMono NF:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Victor Mono:pixelsize=19:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
@@ -117,26 +117,32 @@ float alpha = 0.7;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-    "#45475A", "#F38BA8", "#A6E3A1", "#F9E2AF", "#89B4FA", "#F5C2E7", "#94E2D5",
-    "#BAC2DE",
-
-    /* 8 bright colors */
-    "#585B70", "#F38BA8", "#A6E3A1", "#F9E2AF", "#89B4FA", "#F5C2E7", "#94E2D5",
-    "#A6ADC8",
-
-    [256] = "#CDD6F4", /* default foreground colour */
-    [257] = "#11111B", /* default background colour */
-    [258] = "#F5E0DC", /*575268*/
-
+    [0] = "#141312",  /* hard contrast: #1d2021 / soft contrast: #32302f */
+    [1] = "#cc241d",  /* red     */
+    [2] = "#98971a",  /* green   */
+    [3] = "#d79921",  /* yellow  */
+    [4] = "#458588",  /* blue    */
+    [5] = "#b16286",  /* magenta */
+    [6] = "#689d6a",  /* cyan    */
+    [7] = "#a89984",  /* white   */
+                      /* 8 bright colors */
+    [8] = "#928374",  /* black   */
+    [9] = "#fb4934",  /* red     */
+    [10] = "#b8bb26", /* green   */
+    [11] = "#fabd2f", /* yellow  */
+    [12] = "#83a598", /* blue    */
+    [13] = "#d3869b", /* magenta */
+    [14] = "#8ec07c", /* cyan    */
+    [15] = "#ebdbb2", /* white   */
 };
 
 /*
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 256;
-unsigned int defaultbg = 257;
-unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 258;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
